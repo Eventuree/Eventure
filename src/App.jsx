a8a0c9673b3fd5bfa1ba2882518722d23a081169
Anna Pohorila <annapohorila07@gmail.com>
2026-01-14 14:25:47 +0200

    resolving conflict1

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.jsx";
+import Home from "./pages/Home/Home.jsx";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer.jsx";
 import { AuthProvider } from "./context/AuthContext";
-import { CategoryProvider } from "./context/CategoryContext.jsx";
 import Login from "./pages/Login/Login.jsx";
 import PublicRoute from "./routes/PublicRoute";
 import Register from "./pages/Register/Register.jsx";
+import { RegistrationProvider } from "./context/RegistrationContext.jsx";
 import CreateProfile from "./pages/CreateProfile/CreateProfile.jsx";
+import { CategoryProvider } from "./context/CategoryContext";
 
 function App() {
   return (
     <AuthProvider>
-      <CategoryProvider>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={
-              <PublicRoute>
-                <Login />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <PublicRoute>
-                <Register />
-              </PublicRoute>
-            }
-          />
-          <Route path="/create-profile" element={<CreateProfile/>}/>
-          {/* <Route path="/my-events" element={<MyEvents />} />
+      <RegistrationProvider>
+        <CategoryProvider>
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute>
+                    <Register />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/create-profile"
+                element={
+                  <PublicRoute>
+                    <CreateProfile />
+                  </PublicRoute>
+                }
+              />
+
+              {/* <Route path="/my-events" element={<MyEvents />} />
         <Route path="/registered" element={<Registered />} />
         <Route path="/archived" element={<Archived />} />
         <Route path="/create" element={<CreateEvent />} />
@@ -43,13 +53,12 @@ function App() {
         <Route path="/community-rules" element={<CommunityRules />} />
         <Route path="/faq" element={<FAQ />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register/>} />
         <Route path="/profile" element={<Profile />} />*/}
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-      </CategoryProvider>
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </CategoryProvider>
+      </RegistrationProvider>
     </AuthProvider>
   );
 }
