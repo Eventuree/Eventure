cbb654d9f2464e215e6c8b87ae834884b328795e
Anna Pohorila <annapohorila07@gmail.com>
2026-01-15 18:18:43 +0200

    fixed bugs

--- src/pages/CreateProfile/CreateProfile.jsx
+++ src/pages/CreateProfile/CreateProfile.jsx
@@ -35,32 +35,21 @@ export default function CreateProfile(){
             setPreviewPic(imageUrl)
         }
     }
-  }, [registrationData]);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setProfileData((prev) => ({ ...prev, [name]: value }));
-  };
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file && file.type.startsWith("image/")) {
-      const imageUrl = URL.createObjectURL(file);
-      setPreviewPic(imageUrl);
-    }
-  };
-  const handleAddLink = () => {
-    if (currentLink.trim()) {
-      setSocialLinks([...socialLinks, currentLink]);
-      setCurrentLink("");
-    }
-  };
-  const toggleInterest = (id) => {
-    setSelectedInterests((prev) =>
-      prev.includes(id) ? prev.filter((itemId) => itemId !== id) : [...prev, id]
+    const handleAddLink = () => {
+        if (currentLink.trim()) {
+            setSocialLinks([...socialLinks, currentLink]);
+            setCurrentLink(''); 
+        }
+    };
+    const toggleInterest = (id) => {
+    setSelectedInterests(prev => 
+        prev.includes(id) 
+            ? prev.filter(itemId => itemId !== id) 
+            : [...prev, id]                        
     );
-  };
+};
 
-  const handleSubmit = async (e) => {
+const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (selectedInterests.length === 0) {
@@ -169,116 +158,7 @@ export default function CreateProfile(){
             </fieldset>
             <button type="submit" className={styles.buttonLink}>Створити профіль</button>
 
-            <div className={styles.selectorsContainer}>
-              <div className={styles.citySelector}>
-                <label htmlFor="city" className={styles.labelText}>
-                  Місто
-                </label>
-                <select
-                  id="city"
-                  name="city"
-                  value={profileData.city}
-                  required
-                  className={`${styles.citySelect} ${styles.selectField}`}
-                  onChange={handleChange}
-                >
-                  <option value="" disabled hidden>
-                    Оберіть*
-                  </option>
-                  <option value="kyiv" className={styles.selectOption}>
-                    Київ
-                  </option>
-                  <option value="lviv" className={styles.selectOption}>
-                    Львів
-                  </option>
-                </select>
-              </div>
-              <div className={styles.genderSelector}>
-                <label htmlFor="gender" className={styles.labelText}>
-                  Стать
-                </label>
-                <select
-                  id="gender"
-                  name="gender"
-                  required
-                  value={profileData.gender}
-                  onChange={handleChange}
-                  className={`${styles.genderSelect} ${styles.selectField}`}
-                >
-                  <option value="" disabled hidden>
-                    Оберіть*
-                  </option>
-                  <option value="female" className={styles.selectOption}>
-                    Жіноча
-                  </option>
-                  <option value="male" className={styles.selectOption}>
-                    Чоловіча
-                  </option>
-                  <option value="none" className={styles.selectOption}>
-                    Не хочу вказувати
-                  </option>
-                </select>
-              </div>
-            </div>
-          </div>
-        </fieldset>
-        <fieldset
-          className={`${styles.aboutLinksContainer} ${styles.fieldset}`}
-        >
-          <div className={styles.aboutMe}>
-            <label htmlFor="about-me" className={styles.labelText}>
-              Про себе
-            </label>
-            <textarea
-              id="about-me"
-              placeholder="Про себе"
-              name="aboutMe"
-              value={profileData.aboutMe}
-              onChange={handleChange}
-              className={styles.aboutMeField}
-            ></textarea>
-          </div>
-          <div className={styles.socials}>
-            <label htmlFor="socials" className={styles.labelText}>
-              Соціальні мережі
-            </label>
-            <input
-              type="url"
-              id="socials"
-              placeholder="Посилання на соц. мережі"
-              onChange={handleChange}
-              required
-              className={styles.socialsInput}
-            />
-            <button
-              type="button"
-              className={styles.addMoreButton}
-              onClick={handleAddLink}
-            >
-              + додати ще одне
-            </button>
-          </div>
-        </fieldset>
-        <fieldset
-          className={`${styles.selectInterestsSection} ${styles.fieldset}`}
-        ></fieldset>
-        <fieldset className={`${styles.selectCategories} ${styles.fieldset}`}>
-          <h2 className={styles.title}>Обери інтереси</h2>
-          <div className={styles.interestsContainer}>
-            {categories.map((category) => (
-              <InterestCheckbox
-                key={category.id}
-                category={category}
-                isSelected={selectedInterests.includes(category.id)}
-                onToggle={toggleInterest}
-              />
-            ))}
-          </div>
-        </fieldset>
-        <button type="submit" className={styles.buttonLink}>
-          Створити профіль
-        </button>
-      </form>
-    </section>
-  );
+        </form>
+       </section>
+    )
 }
